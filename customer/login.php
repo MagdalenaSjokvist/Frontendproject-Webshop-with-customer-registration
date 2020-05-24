e7d13755c49a71041ef680495e041112d9b0d0cd
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-05-24 14:05:56 +0200

    Sidrubriken har rätt styling

--- customer/login.php
+++ customer/login.php
@@ -4,18 +4,30 @@ require_once '../config/db.php';
 
 ?>
 
-<div class="login-container">
-  <form action="POST">
-    <div>
+<section class="login-container">
+
+  <h1 class="login-title page-title login-container__title">Välkommen!</h1>
+
+  <form class="login-form" action="#" method="POST">
+    <h2>Logga in</h2>
+
+    <div class="login-form__email form-container__box">
       <label for="email">E-postadress:</label>
       <input type="text" id="email" name="email" required>
     </div>
 
-    <div>
-      <label for="password">Lösenord (8 characters minimum):</label>
+    <div class="login-form__password form-container__box">
+      <label for="password">Lösenord (minst 2 tecken):</label>
       <input type="password" id="password" name="password" minlength="" required>
     </div>
 
-    <input type="submit" value="Sign in">
+    <input type="submit" value="Logga in">
   </form>
-</div>
+
+</section>
+
+<section class="register-container">
+  <h2>Registrera dig</h2>
+</section>
+
+<?php require_once '../footer.php' ?>
